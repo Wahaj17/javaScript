9c027365ce4497681365f6a04a014356d447582f
Syed Ali Wahaj <62831066+Wahaj17@users.noreply.github.com>
2024-07-25 04:45:41 -0700

    Add files via upload

---
+++ The Dice Game/index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1)
+var randomNumber2 = Math.floor((Math.random()*6)+1)
+var rollDice1 = "images/"+randomNumber1+".png";
+var rollDice2 = "images/"+randomNumber2+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", rollDice1)
+document.querySelectorAll("img")[1].setAttribute("src", rollDice2)
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "👈Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!👉";
+} else if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "🤜Draw!🤛";
+}
+
+
+
